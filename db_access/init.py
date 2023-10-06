b836eea2964440b3ffc15ae0d9c5fc530b49c56a
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2023-10-06 12:10:16 +0700

    06/10/1023

--- db_access/init.py
+++ db_access/init.py
@@ -2,24 +2,29 @@ from sqlite3 import connect
 
 
 class db_access():
-    def __init__(self):
+    def __init__(self) -> None:
         self.conn = connect("./database/attendance.db", check_same_thread=False)
         self.cursor = self.conn.cursor()
     
-    def query(self, query, args=()):
+    def fetchone(self, query, *args) -> tuple:
         self.cursor.execute(query, args)
-        if query.startswith("SELECT"):
-            return self.cursor.fetchall()
-        else:
-            self.conn.commit()
+        return self.cursor.fetchone()
 
-    def face_path(self, name=""):
+    def fetchall(self, query, *args) -> list[tuple]:
+        self.cursor.execute(query, args)
+        return self.cursor.fetchall()
+
+    def action(self, query, *args) -> None:
+        self.cursor.execute(query, args)
+        self.conn.commit()
+
+    def face_path(self, name="") -> str:
         if not name:
             return "./database/faces_data"
         if name.endswith(".pkl"):
             return f"./database/faces_data/{name}"
         return f"./database/faces_data/{name}.pkl"
     
-    def str_to_mins(self, s):
+    def str_to_mins(self, s) -> int:
         h, m = map(int, s.split(':'))
         return h*60 + m

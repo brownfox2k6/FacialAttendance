0a178ee74798f9212da414691be52d7fa1cc7adb
Trọng Đại Nguyễn <brfox2k6@gmail.com>
2023-10-06 12:24:45 +0700

    06102023

--- db_access/init.py
+++ db_access/init.py
@@ -1,11 +1,14 @@
+# ./db_access/init.py
+
 from sqlite3 import connect
 
 
 class db_access():
     def __init__(self) -> None:
-        self.conn = connect("./database/attendance.db", check_same_thread=False)
+        self.conn = connect("./database/attendance.db",
+                            check_same_thread=False)
         self.cursor = self.conn.cursor()
-    
+
     def fetchone(self, query, *args) -> tuple:
         self.cursor.execute(query, args)
         return self.cursor.fetchone()
@@ -24,7 +27,7 @@ class db_access():
         if name.endswith(".pkl"):
             return f"./database/faces_data/{name}"
         return f"./database/faces_data/{name}.pkl"
-    
+
     def str_to_mins(self, s) -> int:
         h, m = map(int, s.split(':'))
         return h*60 + m
